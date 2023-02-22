853475d964f212497883f9afc7286a350ad90e4f
VeselinDimitrov3 <codyblade4@gmail.com>
2023-02-22 19:43:13 +0200

    new commit

--- src/main/java/com/example/OldButGold/OldButGoldApplication.java
+++ src/main/java/com/example/OldButGold/OldButGoldApplication.java
@@ -1,10 +1,18 @@
 package com.example.OldButGold;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class OldButGoldApplication {
+
+//	@Bean
+//	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//		return new BCryptPasswordEncoder();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OldButGoldApplication.class, args);
